8d851e86b6b5bad1225f1947cc6d59936538543b
wypchlo <deron.pl.yt@gmail.com>
2024-09-16 21:18:44 +0200

    General code cleanup and minor optimisations

--- desktop_apps/labs/lab2/3d_arr_interp.cpp
+++ desktop_apps/labs/lab2/3d_arr_interp.cpp
@@ -4,7 +4,7 @@
 #include<cmath>
 #include<iomanip>
 
-void print_2d_table(float** arr, int size_x, int size_y) {
+void print_2d_table(float** arr, short size_x, short size_y) {
     std::cout << std::endl << '[' << std::endl;
     for (int y = 0; y < size_y; y++) {
         std::cout << " |";
@@ -19,22 +19,20 @@ float** interpolate_2d_array(float** arr, short width, short height, short inter
     for (int y = 0; y <= (height - 1) * interp; y++) interp_array[y] = new float[width * interp];
 
     // interpolates even horizontal lines
-     for (int y = 0; y <= (height - 1) * interp; y+=interp) {
+     for (int y = 0; y <= (height - 1) * interp; y += interp) {
         for (int x = 0; x <= (width - 1) * interp; x++) {
-            float step = x / (float) interp;
-            short floor = std::floor(step);
-            short ceil = std::ceil(step);
+            short floor = x / interp;
+            short ceil = (x % interp == 0) ? floor : floor + 1;
             short array_y = y / interp;
             interp_array[y][x] = arr[array_y][floor] - (arr[array_y][floor] - arr[array_y][ceil]) * (x % interp / (float) interp);
         }   
     }
 
     // interpolates even vertical lines
-    for (int x = 0; x <= (width - 1) * interp; x+=interp) {
+    for (int x = 0; x <= (width - 1) * interp; x += interp) {
         for (int y = 1; y <= interp * (height - 1); y++) {
-            float step = y / (float) interp;
-            short floor = std::floor(step);
-            short ceil = std::ceil(step);
+            short floor = y / interp;
+            short ceil = (y % interp == 0) ? floor : floor + 1;
             short array_x = x / interp;
             interp_array[y][x] = arr[floor][array_x] - (arr[floor][array_x] - arr[ceil][array_x]) * (y % interp / (float) interp);
         }
@@ -43,10 +41,9 @@ float** interpolate_2d_array(float** arr, short width, short height, short inter
     // interpolates even diagonal lines
     for (int y = 1; y <= (height - 1) * interp; y++) {
         short val = y % interp;
-        for (int x = val; x <= (width - 1) * interp; x+=interp) {
-            float step = y / (float) interp;
-            short floor = std::floor(step);
-            short ceil = std::ceil(step);
+        for (int x = val; x <= (width - 1) * interp; x += interp) {
+            short floor = y / interp;
+            short ceil = (y % interp == 0) ? floor : floor + 1;
             short array_x = x / interp;
             interp_array[y][x] = arr[floor][array_x] - (arr[floor][array_x] - arr[ceil][array_x + 1]) * (y % interp / (float) interp);
         }
@@ -65,8 +62,7 @@ float** interpolate_2d_array(float** arr, short width, short height, short inter
 
                 for (int box_x = box_y + 1; box_x < interp; box_x++) {
                     interp_array[y][j * interp + box_x] = 
-                        interp_array[y][j * interp + box_y] - (interp_array[y][j * interp + box_y] - interp_array[y][j * interp + interp]) * 
-                        ((box_x - box_y) / (float) (interp - box_y));
+                        interp_array[y][j * interp + box_y] - (interp_array[y][j * interp + box_y] - interp_array[y][j * interp + interp]) * ((box_x - box_y) / (float) (interp - box_y));
                 }
             }
         }
@@ -77,36 +73,33 @@ float** interpolate_2d_array(float** arr, short width, short height, short inter
 
 int main(void) {
     std::srand(std::time(0));
-    
-    int size_x;
-    std::cout << "Podaj szerokość tablicy: ";
-    std::cin >> size_x;
 
-    int size_y;
-    std::cout << "Podaj wysokość tablicy: ";
-    std::cin >> size_y;
+    short width;
+    std::cout << "Podaj szerokość tablicy: ", std::cin >> width;
 
-    float** array = new float*[size_y];
-    for (int i = 0; i < size_y; i++) array[i] = new float[size_x];
+    short height;
+    std::cout << "Podaj wysokość tablicy: ", std::cin >> width;
+
+    float** array = new float*[width];
+    for (int i = 0; i < height; i++) array[i] = new float[width];
 
     std::cout << "Wygenerowana tablica to:" << std::endl;
-    for (int y = 0; y < size_y; y++)
-        for (int x = 0; x < size_x; x++) array[y][x] = rand() % 10;
+    for (int y = 0; y < height; y++)
+        for (int x = 0; x < width; x++) array[y][x] = rand() % 10;
 
-    print_2d_table(array, size_x, size_y);
+    print_2d_table(array, width, height);
     
     short interp;
-    std::cout << "Podaj poziom interpolacji tablicy: ";
-    std::cin >> interp;
+    std::cout << "Podaj poziom interpolacji tablicy: ", std::cin >> interp;
 
-    float** interp_array = interpolate_2d_array(array, size_x, size_y, interp);
+    float** interp_array = interpolate_2d_array(array, width, height, interp);
 
-    print_2d_table(interp_array, interp * (size_x - 1) + 1, interp * (size_y - 1) + 1);
+    //print_2d_table(interp_array, interp * (size_x - 1) + 1, interp * (size_y - 1) + 1);
 
-    for (int i = 0; i < size_y; i++) delete [] array[i];
+    for (int i = 0; i < height; i++) delete [] array[i];
     delete [] array;
 
-    for (int i = 0; i < size_y * interp; i++) delete [] interp_array[i];
+    for (int i = 0; i < height * interp; i++) delete [] interp_array[i];
     delete [] interp_array;
 
     return 0;
