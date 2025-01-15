31b9798694a913f9793995ca237d6387bdf82184
wypchlo <deron.pl.yt@gmail.com>
2025-01-15 14:25:44 +0100

    b

---
+++ PAW/12/12/app/components/BlockButton.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+export default function BlockButton({onClick, children}: any) {
+    const playClickSound = () => {
+        let clickAudio = new Audio("/click.mp3");
+        clickAudio.play();
+    }
+
+    const handleClick = (e: any) => {
+        playClickSound();
+        if(onClick) {
+            onClick(e);
+        }
+    }
+
+    return (
+        <button onClick={handleClick} className="blockButton">
+            { children } 
+        </button>
+    ) 
+}
---
+++ PAW/12/12/app/routes/home.tsx
@@ -0,0 +1,73 @@
+import logoImage from "/logobest.png";
+import React, { useRef, useEffect } from "react";
+import BlockButton from "app/components/BlockButton";
+
+export function meta() {
+    return [
+        { title: "Minecraft forum" },
+        { name: "description", content: "Welcome to React Router!" },
+    ];
+}
+
+export default function Home() {
+    function removeCharacterAtIndex(str: String, index: number) {
+        if (index < 0 || index >= str.length) {
+            return str;
+        }
+        return str.slice(0, index) + str.slice(index + 1);
+    }
+
+    function scrambleString(s: String) {
+        s = s.slice(1, s.length - 1);
+        let result: String = "";
+
+        while(true) {
+            if(s.length == 0) break;
+            let index = Math.floor(Math.random() * s.length);
+            result += s[index];
+            s = removeCharacterAtIndex(s, index);
+        }
+
+        return result;
+    }
+
+    function scramble() {
+        let initial: String = getComputedStyle(document.documentElement).getPropertyValue('--logo-message');
+        document.documentElement.style.setProperty('--logo-message', `'${scrambleString(initial)}'`);
+    }
+    
+    const audio = useRef<HTMLAudioElement>(null);
+
+    function toggleMusic() {
+        if(!audio.current!.paused) return audio.current!.pause();
+        audio.current!.play();
+    }
+    
+    useEffect(() => {
+        audio.current = new Audio("/calm1.ogg");
+        audio.current.currentTime += 10;
+    });
+
+    return (
+        <main id="home">
+            <div id="routeIndicator"> Strona glówna </div>
+
+            <header>
+                <div><img src={logoImage} id="logoImage"/></div>
+            </header>
+            
+            <nav>
+                <div id="container">
+                    <BlockButton> Wpisy </BlockButton>
+                    <button> Kategorie </button>
+                    <div id="bonus">
+                        <button onClick={scramble}> Scramble </button>
+                        <button onClick={toggleMusic}> Music </button>
+                    </div>
+                </div>
+            </nav>
+            
+            <footer> Copyright Milosz Wypchlo. Do not distribute. </footer>
+        </main>
+    );
+}
