0ca8df1561d46e929b967012b52ae84ff1039930
wypchlo <deron.pl.yt@gmail.com>
2025-01-18 20:04:27 +0100

    added cors

--- PAW/11/7_tsc/prisma/migrations/20250118143148_idk/migration.sql
+++ PAW/11/7_tsc/prisma/migrations/20250118143148_idk/migration.sql
@@ -1,5 +1,12 @@
--- AlterTable
-ALTER TABLE `post` MODIFY `description` VARCHAR(191) NULL DEFAULT '';
+-- CreateTable
+CREATE TABLE `post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL DEFAULT '',
+    `content` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `comment` (
--- PAW/11/7_tsc/src/app.ts
+++ PAW/11/7_tsc/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import mainRouter from "./routes";
+const cors = require("cors");
 
 const PORT: number = 3000;
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', mainRouter);
